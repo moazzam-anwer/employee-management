60da14fc9b5d09f716ddd431b183f9484ef2245f
neosoft <moazzam.anwer@neosoftmail.com>
2024-08-30 17:19:50 +0530

    api fixed

---
+++ src/app/@footer/default.tsx
@@ -0,0 +1,15 @@
+
+
+const defaultFooter = () => {
+    return (
+        <div className="footer-container"> 
+            <footer>
+                <p>Copyright 2024 Employee Management Application. All rights reserved.</p>
+            </footer>
+            
+        </div>
+    );
+};
+
+export default defaultFooter;
+
---
+++ src/app/@header/layout.tsx
@@ -0,0 +1,45 @@
+
+'use client'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation';
+import { useState,useEffect } from "react";
+import Navbars from '../components/NavBar';
+
+
+const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+};
+
+export default function HeaderLayout() {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if token is present in cookies
+    const token = getCookie('token');
+    setIsLoggedIn(!!token);
+  }, [router]);
+
+  const handleLogout = async () => {
+    
+    setIsLoggedIn(false)
+    // Clear JWT token from cookies
+    document.cookie = 'token=; Max-Age=0; path=/'; // Clear the cookie
+  
+    // Redirect to login page
+    router.push('/login');
+  };
+  return (
+    <header>
+      <Image src={`/images/logo.webp`} alt="logo" width="64" height="64" />
+      <div className='header-container'>
+        <h1>Employee Management Applications</h1>
+        {isLoggedIn && <button className="btn-logout" onClick={handleLogout}>Logout</button>}
+      </div>
+     <Navbars />
+    </header>
+  );
+}
